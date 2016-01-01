eff8bb75ce6f8de043268f0bdb85d01bcfc360ee
opl- <opl-@users.noreply.github.com>
2016-01-01 14:34:04 +0100

    Add bot mode + Fix script sending 50 requests/15 seconds when rate limited

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 app-list.txt thanks to xPaw (http://xpaw.me)
+
+How do I use this?
+---
+1. Get [Node.js](https://nodejs.org/)
+2. Pull this script (or just download it as a zip file [from here](https://github.com/opl-/winter-arg-tester/archive/master.zip) and unpack it somewhere).
+3. Open a console in the directory you downloaded the code to.
+4. Run:
+  * `node cli help` to print the help message.
+  * `node cli password <password>` where `<password>` is the phrase you want to test to check it for all games.
+  * `node cli bot` to run in bot mode, which automatically gets passwords from a database and tests them without you having to do anything.
--- cli.js
+++ cli.js
@@ -1,16 +1,14 @@
 var tester = require('./main');
 var fs = require('fs');
+var https = require('https');
 
-if (process.argv.length < 4 || process.argv[2] === 'help') {
-	console.log('== help');
-	console.log('Prints this message.');
-	console.log('== password <password>');
-	console.log('Check <password> against all app ids listed in app-list.txt. Don\'t use quotes!');
-} else if (process.argv[2] === 'password') {
+var remote = 'warg.ngrok.io';
+
+function checkPassword(password, callback) {
 	var appIDs = fs.readFileSync('app-list.txt').toString().replace(/\r/g, '').split('\n');
 	var currentAppID = 0;
 
-	var password = process.argv.slice(3, process.argv.length).join(' ');
+	var results = [];
 
 	var queries = 0;
 	var elapsed = 0;
@@ -18,8 +16,10 @@ if (process.argv.length < 4 || process.argv[2] === 'help') {
 
 	var done = 0;
 
+	console.log('== Checking password: ' + password);
+
 	console.log();
-	setInterval(function() {
+	var intervalID = setInterval(function() {
 		elapsed++;
 		process.stdout.write('\x1b[1000D\x1b[K\x1b[A');
 		console.log((done + '/' + appIDs.length + '        ').substr(0,10) + ' ' + (queries + 'q/s       ').substr(0,8) + ' ' + (elapsed + 's     ').substr(0,5) + ' ' + (rateLimited ? '! RATE LIMITED (waiting) !' : ''));
@@ -27,7 +27,8 @@ if (process.argv.length < 4 || process.argv[2] === 'help') {
 
 		if (done === appIDs.length) {
 			console.log('Done!');
-			return process.exit();
+			clearInterval(intervalID);
+			return callback(results);
 		}
 	}, 1000);
 
@@ -36,12 +37,12 @@ if (process.argv.length < 4 || process.argv[2] === 'help') {
 		process.exit();
 	});
 
-	function nextAppID() {
+	function nextAppID(ignoreRateLimit, fromTimeout) {
 		if (currentAppID === appIDs.length) return;
 
-		if (rateLimited) {
+		if (rateLimited && !fromTimeout) {
 			return setTimeout(function() {
-				nextAppID();
+				nextAppID(ignoreRateLimit, ignoreRateLimit);
 			}, 15000);
 		}
 
@@ -56,18 +57,121 @@ if (process.argv.length < 4 || process.argv[2] === 'help') {
 				currentAppID--;
 			} else {
 				rateLimited = false;
-				if (!result) {
-				} else if (result.url) {
-					console.log('[found url] app=' + appID + ', password=' + password + ', url=' + result.url + '\n');
-				} else if (result.response) {
-					console.log('[found response] app=' + appID + ', password=' + password + ', response=' + result.response + '\n');
-				} else {
-					console.log('[found weird] app=' + appID + ', password=' + password + ', result=' + JSON.stringify(result) + '\n');
+				if (result) {
+					if (result.url) {
+						console.log('[found url] app=' + appID + ', password=' + password + ', url=' + result.url + '\n');
+					} else if (result.response) {
+						console.log('[found response] app=' + appID + ', password=' + password + ', response=' + result.response + '\n');
+					} else {
+						console.log('[found weird] app=' + appID + ', password=' + password + ', result=' + JSON.stringify(result) + '\n');
+					}
+
+					results.push({
+						appID: appID,
+						result: result
+					});
 				}
-				nextAppID();
+				nextAppID(ignoreRateLimit);
 			}
 		});
 	}
 
-	for (var w = 0; w < 50; w++) nextAppID();
+	for (var w = 0; w < 50; w++) nextAppID(w === 0);
+}
+
+function printHelp() {
+	console.log('== help');
+	console.log('Prints this message.');
+	console.log('== password <password>');
+	console.log('Check <password> against all app ids listed in app-list.txt. Don\'t use quotes!');
+	console.log('== bot');
+	console.log('Automatically checks passwords.');
+}
+
+if (process.argv.length < 3 || process.argv[2] === 'help') {
+	printHelp();
+} else if (process.argv[2] === 'password') {
+	if (process.argv.length < 3) return printHelp();
+
+	checkPassword(process.argv.slice(3, process.argv.length).join(' '), function(result) {
+		process.exit();
+	});
+} else if (process.argv[2] === 'bot') {
+	function postResults(resp, result) {
+		var req = https.request({
+			host: remote,
+			path: '/solve',
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			}
+		}, function(res) {
+			res.on('data', function(data) {
+				req.response = (req.response || '') + data.toString();
+			}).on('end', function() {
+				try {
+					var a = JSON.parse(req.response);
+					if (a.status === 'success') {
+						console.log('Results posted.');
+						getNextPassword();
+					} else {
+						console.warn('Error posting results! Retrying in 10 seconds.');
+						console.log(req.response);
+						setTimeout(function() {
+							postResults(resp, result);
+						}, 10000);
+					}
+				} catch (e) {
+					console.log('Exception when posting results. Retrying in 10 seconds.', e);
+					console.log(req.response);
+					setTimeout(function() {
+						postResults(resp, result);
+					}, 10000);
+				}
+			});
+		});
+
+		req.write(JSON.stringify({
+			id: resp.id,
+			result: result
+		}))
+
+		req.end();
+	}
+
+	function getNextPassword() {
+		var req = https.request({
+			host: remote,
+			path: '/nextpassword',
+			headers: {
+				'content-type': 'application/json'
+			}
+		}, function(res) {
+			res.on('data', function(data) {
+				req.response = (req.response || '') + data.toString();
+			}).on('end', function() {
+				try {
+					var resp = JSON.parse(req.response);
+
+					if (resp.status === 'success') {
+						checkPassword(resp.password, function(result) {
+							postResults(resp, result);
+						});
+					} else if (resp.status === 'queue_empty') {
+						console.log('No passwords to check. Retrying in 10 seconds.');
+						setTimeout(getNextPassword, 10000);
+					} else {
+						console.log('Unknown response (retrying in 10 seconds):', resp);
+						setTimeout(getNextPassword, 10000);
+					}
+				} catch (e) {
+					console.log('Exception when getting next password. Retrying in 10 seconds.', e);
+					setTimeout(getNextPassword, 10000);
+				}
+			});
+		});
+		req.end();
+	}
+
+	getNextPassword();
 }
