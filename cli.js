80ba17ce8b5004917241263d2be229cddd9cc848
C-at <danv08@yahoo.com>
2016-01-03 22:15:14 -0600

    Update cli.js
    Fixed help command

--- cli.js
+++ cli.js
@@ -103,6 +103,8 @@ function printHelp() {
 	console.log('Prints this message.');
 	console.log('== password <password>');
 	console.log('Check <password> against all app ids listed in a text file (default app-list.txt). Don\'t use quotes!');
+	console.log('== list');
+	console.log('Bruteforces all passwords in a text file (defaul custom-list.txt).');
 	console.log('== bot');
 	console.log('Automatically checks passwords.');
 }
